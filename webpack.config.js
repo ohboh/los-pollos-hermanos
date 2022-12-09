40cb5f982ba11000d4fe1ad16c0d3b9e2622146b
ohboh <depafoboe@gmail.com>
2022-12-10 02:30:59 +0800

    Add file loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output:{
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+        {
+            test: /\.(mp4)$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]'
+                }  
+              }
+            ]
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+              {
+                loader: 'file-loader', 
+                options: {
+                    name: "[name].[ext]"
+                }
+            }
+            ]
+          },
+    ],
   },
 };
